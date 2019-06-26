d59107167974ca7a9c821da2a5c1b013897b2b8b
KarolaynMazo <Karolaynmazo@gmail.com>
2019-06-26 11:49:59 -0500

    examen

---
+++ src/App/components/SecondExam/SecondExam.js
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <React.Fragment>
+
+            <h1 className="animated fadeIn fast">Segundo examen (16%)</h1>
+            <hr />
+            <p className="animated fadeIn">
+                <p><h2 className="animated fadeIn fast">Indicaciones:</h2></p>
+                <div>
+                    <ol>
+                        <li>Descargar todo el proyecto de Heroes-React.</li>
+                        <li>En este espacio, usted debe crear una página que tenga que ver con su proyecto de clase.</li>
+                        <li>Entre los integrantes del proyecto no pueden existir paginas iguales, ni similares.</li>
+                        <li>El proyecto completo debe adicionarse al GitHub.</li>
+                        <li>En un documento ubicado en el Google Drive cada uno escribirá el enlace suministrado por GitHub para una posterior descarga.</li>
+                        <li>El contenido de la página como mínimo debe tener algo similar a la página de "Heroes"; es decir, varias imagenes, enlaces, divisiones, lo que usted considere pertinente para ganarse un 5.0.</li>
+                        <li>A las 12 m. del día de hoy debe estar el enlace en el Google Drive.</li>
+                    </ol>
+                </div>            </p>
+ 
+ 
+   <h1 aling ="center">INTERIOR DESIGN KYM </h1>
+   <h2>PRODUCTOS Y PROVEEDORES.</h2> 
+
+   <p>Como uno de nuestros enfoques es facilarte a ti como usuario el acceso a los elementos vistos en el diseños 
+            tenemos algunos lugares que puedes visitar, te seran de gran utilidad en la experiencia que quieres generar para tu inmueble.
+
+          </p>
+          <h2>Como uno de nuestros primcipales proveedores tendremos:</h2>
+          <li>Tenemos HOMECENTER: Pinturas y ambientes para inspirarte, haciendo mas agradables tus espacios de trabajo.
+               </li>
+              
+         
+          <img src="img/homecenter.jpg"></img>
+          <p>
+
+              El color que escojamos para pintar nuestro hogar es muy importante. Los colores nos dan sensaciones y emociones, algunas tristes y otras muy alegres. 
+              Por eso es importante usar el color de acuerdo al perfil que queramos darle a el espacio.
+          </p>
+
+        <li>Tenemos FALABELLA: Muebles y accesorios.
+
+               </li>
+               <img src="img/falabella.jpg"></img>
+               <p>
+              Si quieres renovar la energía de tu casa, te proponemos una técnica tradicional con la que lograrás equilibrar y armonizar tu hogar.
+               El Feng Shui procura transformar los espacios a fin de que te sientas bien y a gusto, respetando tu personalidad. 
+          </p>
+
+          
+        <li>Tenemos PAVEZGO: Iluminación y creatividad.
+
+</li>
+<img src="img2/pavezgo.jpg"></img>
+<p>
+              En muchas ocasiones la luz juega un papel fundamental en nuestros hogares. Añadir un punto de luz en un lugar estratégico puede sumar visualmente metros de amplitud a nuestras estancias. Además, la luz influye en nuestro estado de ánimo
+               por lo que es clave para decorar un estudio, un comedor o una habitación siguiendo pautas diferentes.
+          </p>
+
+
+         
+
+   
+        </React.Fragment>
+    )
+}
+
+export default About
